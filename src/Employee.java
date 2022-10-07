902e93b63d9c168de2c287d737108fb713f3f16f
esadbzkrt <esadbzkrt@gmail.com>
2022-10-08 01:40:59 +0300

    Completed

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Employee {
+
+public class Employee {
+
+    private final String name;
+    private final double salary;
+    private final int workHours;
+    private final int hireYear;
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public double tax() {
+        if (this.salary < 1000) {
+            return 0;
+        } else {
+            return this.salary * 0.03;
+        }
+    }
+
+    public double bonus() {
+        if (this.workHours > 40) {
+            return (this.workHours - 40) * 30;
+        } else {
+            return 0;
+        }
+    }
+
+    public double raiseSalary() {
+        int year = 2021;
+        if (year - this.hireYear < 10) {
+            return this.salary * 0.05;
+        } else if (year - this.hireYear > 9 && year - this.hireYear < 20) {
+            return this.salary * 0.10;
+        } else {
+            return this.salary * 0.15;
+        }
+    }
+
+    public String toString() {
+        return "Name: " + this.name + ", Salary: " + this.salary + ", Work Hours: " + this.workHours + ", Hire Year: " + this.hireYear;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        public static void main(String[] args) {
+            Employee employee1 = new Employee("Ali", 2000, 45, 1985);
+            Employee employee2 = new Employee("Veli", 3000, 35, 1995);
+            Employee employee3 = new Employee("Ayse", 2500, 40, 2005);
+            Employee employee4 = new Employee("Fatma", 2200, 37, 2010);
+            Employee employee5 = new Employee("Kemal", 2750, 42, 1975);
+
+            Employee[] employees = {employee1, employee2, employee3, employee4, employee5};
+
+
+            for (Employee employee : employees) {
+                System.out.println(employee);
+                System.out.println("Tax: " + employee.tax());
+                System.out.println("Bonus: " + employee.bonus());
+                System.out.println("Raise Salary: " + employee.raiseSalary());
+                System.out.println("----------------------------");
+            }
+        }
+
+
 }
